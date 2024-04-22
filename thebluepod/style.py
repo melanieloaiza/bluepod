78c36e4e5e008fe60634efa589d4d7c630a07502
Mel Loaiza <72766590+melanieloaiza@users.noreply.github.com>
2024-04-21 22:23:03 -0400

    Add files via upload

---
+++ thebluepod/style.py
@@ -0,0 +1,28 @@
+css = """
+    <style>
+    .gradient-text {
+      font-size: 55px;
+      font-weight: bold;
+      background: -webkit-linear-gradient(60deg, #0575e6, #00ffb0);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+    } 
+    .gradient-subtitle {
+      font-size: 25px;
+      font-weight: bold;
+      background: -webkit-linear-gradient(60deg, #0575e6, #00ffb0);
+      background-clip: text;
+      -webkit-background-clip: text;
+      color: transparent;
+    }
+    .text-justify {
+     text-align: justify;
+    }
+    .small-header {
+    font-size: 18px; 
+    font-weight: bold;
+    }
+    </style>
+"""
+
